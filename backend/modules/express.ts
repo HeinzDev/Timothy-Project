42d9c50fe84cec95e82f01557fbd96cdf3c5bdfb
HeinzDev <heinz1dev@gmail.com>
2023-10-21 21:19:10 -0300

    vite branch first commit

--- backend/modules/express.ts
+++ backend/modules/express.ts
@@ -50,196 +50,184 @@ app.get('/verificar-autenticacao', verifyToken, async (req:any, res:any) => {
 
   */
 
-
-
-const frontendBuildPath = path.join(__dirname, '../../frontend/build');
+const frontendBuildPath = path.join(__dirname, '../../frontend/dist');
 
 //se for SinglePageApp, pegar todas as views e colocar identica a essa abaixo (mudando a rota "/")
-app.use("/static", express.static(path.join(frontendBuildPath, "static")));
-app.get("/", (req: any, res: any) => {
-    const indexPath = path.join(frontendBuildPath, "index.html");
-    console.log("Sending index.html from path:", indexPath);
-    res.sendFile(indexPath);
-  });
-  
+app.use('/static', express.static(path.join(frontendBuildPath, 'static')));
+app.get('/', (req: any, res: any) => {
+  const indexPath = path.join(frontendBuildPath, 'index.html');
+  console.log('Sending index.html from path:', indexPath);
+  res.sendFile(indexPath);
+});
 
 // GETS
-app.get('/users', async (req:any, res:any)=>{
-    try {
-        const users = await UserModel.find({});
+app.get('/users', async (req: any, res: any) => {
+  try {
+    const users = await UserModel.find({});
 
-        res.status(200).json(users); 
-    } catch (error: any) {
-        return res.status(500).send(error.message);
-    }
-
-});
- 
-app.get("/users/:id", async (req:any, res:any) =>{
-    try {
-        const id = req.params.id;
-        const user = await UserModel.findById(id);
-
-        return res.status(200).json(user); 
-    } catch (error: any) {
-        return res.status(500).send(error.message);
-    }
+    res.status(200).json(users);
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
 });
 
-app.get("/games", async (req:any, res:any)=>{
-    try {
-        const posts = await GamesModel.find({})
+app.get('/users/:id', async (req: any, res: any) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
 
-        res.status(200).json(posts)
-    } catch (error: any) {
-        return res.status(500).send(error.message);
-    }
-})
+    return res.status(200).json(user);
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
+});
 
-app.get("/games/:id", async (req:any, res:any)=>{
-    try {
-        const id = req.params.id;
-        const post = await GamesModel.findById(id);
+app.get('/games', async (req: any, res: any) => {
+  try {
+    const posts = await GamesModel.find({});
 
-        return res.status(200).json(post); 
-    } catch (error: any) {
-        return res.status(500).send(error.message);
-    }
-})
+    res.status(200).json(posts);
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
+});
 
-app.get("/games/:id/comments", async(req:any, res:any)=>{
-    try {
-        const postId = req.params.id;
-        const comments = await CommentModel.find({ postId: postId });
-    
-        res.status(200).json(comments);
-    } catch (error: any) {
-        res.status(500).send(error.message);
-    }
-})
+app.get('/games/:id', async (req: any, res: any) => {
+  try {
+    const id = req.params.id;
+    const post = await GamesModel.findById(id);
+
+    return res.status(200).json(post);
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get('/games/:id/comments', async (req: any, res: any) => {
+  try {
+    const postId = req.params.id;
+    const comments = await CommentModel.find({ postId: postId });
+
+    res.status(200).json(comments);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
 
 // POSTS
 
 // Verificar se o sistema precisa (criar usuários pra gente)
-app.post('/users', async (req:any, res:any) =>{
-    try{
-        const user = await UserModel.create(req.body);
+app.post('/users', async (req: any, res: any) => {
+  try {
+    const user = await UserModel.create(req.body);
 
-        res.status(201).json(user);
-    }
-    catch(error: any){
-        res.status(500).send(error.message);
-    }
+    res.status(201).json(user);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
 
+app.post('/games', async (req: any, res: any) => {
+  try {
+    const post = await GamesModel.create(req.body);
 
-app.post('/games', async (req:any, res:any)=>{
-    try {
-        const post = await GamesModel.create(req.body);
+    res.status(201).json(post);
+  } catch (error: any) {
+    return res.status(500).send(error.message);
+  }
+});
 
-        res.status(201).json(post);
-    } catch (error: any) {
-        return res.status(500).send(error.message);
-    }
-})
+app.post('/games/:id/comments', async (req: any, res: any) => {
+  try {
+    const postId = req.params.id;
+    const comment = await CommentModel.create({
+      text: req.body.text,
+      postId: postId,
+    });
 
-app.post('/games/:id/comments', async (req:any, res:any) => {
-    try {
-      const postId = req.params.id;
-      const comment = await CommentModel.create({
-        text: req.body.text,
-        postId: postId,
-      });
-  
-      res.status(201).json(comment);
-    } catch (error: any) {
-      res.status(500).send(error.message);
-    }
+    res.status(201).json(comment);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
 
 // PATCHS
 
-app.patch('/users/:id', async (req:any, res:any) =>{
-    try {
-        const id = req.params.id;
-        const users = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+app.patch('/users/:id', async (req: any, res: any) => {
+  try {
+    const id = req.params.id;
+    const users = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
 
-        res.status(200).json(users);
-    } catch (error: any) {
-        res.status(500).send(error.message);
-    }
-})
-
-app.patch('/games/:id', async (req:any, res:any) => {
-    try {
-      const id = req.params.id;
-      const post = await GamesModel.findByIdAndUpdate(id, req.body, { new: true });
-
-      if (!post) {
-        return res.status(404).json({ error: 'Post not found' });
-      }
-  
-      res.status(200).json(post);
-    } catch (error: any) {
-      res.status(500).send(error.message);
-    }
+    res.status(200).json(users);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.patch('/games/:postId/comments/:commentId', async (req:any, res:any) => {
-    try {
-      const postId = req.params.postId;
-      const commentId = req.params.commentId;
-  
-      const updatedComment = await CommentModel.findByIdAndUpdate(
-        commentId,
-        req.body,
-        { new: true }
-      );
-  
-      res.status(200).json(updatedComment);
-    } catch (error: any) {
-      res.status(500).send(error.message);
+app.patch('/games/:id', async (req: any, res: any) => {
+  try {
+    const id = req.params.id;
+    const post = await GamesModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
     }
-  });
+
+    res.status(200).json(post);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch('/games/:postId/comments/:commentId', async (req: any, res: any) => {
+  try {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+
+    const updatedComment = await CommentModel.findByIdAndUpdate(commentId, req.body, { new: true });
+
+    res.status(200).json(updatedComment);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
 
 // DELETES
 
-app.delete('/users/:id', async (req:any, res:any)=>{
-    try {
-        const id = req.params.id;
-        const user = await UserModel.findByIdAndRemove(id);
+app.delete('/users/:id', async (req: any, res: any) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
 
-        res.status(200).json(user);
-    } catch (error: any) {
-        res.status(500).send(error.message);
-    }
-})
-
-app.delete('/games/:id', async (req:any, res:any)=>{
-    try {
-        const id = req.params.id;
-        const user = await GamesModel.findByIdAndRemove(id);
-
-        res.status(200).json(user);
-    } catch (error: any) {
-        res.status(500).send(error.message);
-    }
-})
-
-app.delete('/games/:postId/comments/:commentId', async (req:any, res:any) => {
-    try {
-      const commentId = req.params.commentId;
-  
-      await CommentModel.findByIdAndRemove(commentId);
-  
-      res.status(204).send();
-    } catch (error: any) {
-      res.status(500).send(error.message);
-    }
+    res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
 
+app.delete('/games/:id', async (req: any, res: any) => {
+  try {
+    const id = req.params.id;
+    const user = await GamesModel.findByIdAndRemove(id);
 
+    res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete('/games/:postId/comments/:commentId', async (req: any, res: any) => {
+  try {
+    const commentId = req.params.commentId;
+
+    await CommentModel.findByIdAndRemove(commentId);
+
+    res.status(204).send();
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`running on port:${process.env.PORT || port}`);
+  console.log(`running on port:${process.env.PORT || port}`);
 });

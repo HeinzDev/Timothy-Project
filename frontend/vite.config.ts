ab6c6ffa848d9cd2b99528ac61f8121e609f1a90
HeinzDev <heinz1dev@gmail.com>
2023-10-21 22:39:22 -0300

    Fixed vite basepath

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  base: '/frontend/',
+  base: './',
   plugins: [react()],
 });

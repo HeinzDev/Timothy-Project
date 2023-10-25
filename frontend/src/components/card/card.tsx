b56197c0182913768ae28b05a760fb495c6d4b39
Enzo Viana Siqueira <enzo.siqueira@ebc.com.br>
2023-10-25 14:48:41 -0300

    Implemented 3d Card Effect

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './card.css';
 
 interface CardProps {
-    name:string,
-    img:string
+  name: string;
+  img: string;
 }
 
-//Reactfc significa react funcional component
-const Card: React.FC<CardProps> = ({name,img}) => {
+const Card: React.FC<CardProps> = ({ name, img }) => {
+  const cardRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    const card = cardRef.current;
+
+    if (card) {
+      const cardRect = card.getBoundingClientRect();
+
+      // Calcula a posição do mouse relativa ao centro do card
+      const mouseX = e.clientX - cardRect.left - cardRect.width / 2;
+      const mouseY = e.clientY - cardRect.top - cardRect.height / 2;
+
+      // Calcula a rotação com base na posição do mouse
+      const rotationX = (mouseY / cardRect.height) * 40;
+      const rotationY = -(mouseX / cardRect.width) * 40;
+
+      // Aplica a rotação ao card
+      card.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    }
+  };
+
+  const handleMouseLeave: React.MouseEventHandler<HTMLDivElement> = () => {
+    const card = cardRef.current;
+    if (card) {
+      card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    }
+  };
+
   return (
-    <div className='item' key={name}>
-    {name}
-        <div className="card">
-            <img src={img} alt='game-card' className='card-media'/>
-        </div>
+    <div className='item'>
+      {name}
+      <div className="card-overlay"></div>
+      <div className="card" ref={cardRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+        <img src={img} alt='game-card' className='card-media' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- frontend/src/components/games/games.tsx
+++ frontend/src/components/games/games.tsx
@@ -9,7 +9,10 @@ export const Games = () => {
         {name:'Catherine: Fullbody',img:'https://m.media-amazon.com/images/I/815BAHe2-lL.jpg'},
         {name:'The killer is dead',img:'https://m.media-amazon.com/images/I/71MW+LprsjL._AC_UF1000,1000_QL80_.jpg'},
         {name:'The Legend of Zelda: Ocarina of time',img:'https://neverendingrealm.com/wp-content/uploads/2019/02/The-Legend-of-Zelda-Ocarina-of-Time-3D-Box-Art.jpg'},
-        {name:'Castlevania: Symphony of the Night',img:'https://assets-prd.ignimgs.com/2021/12/14/castlevaniasymp-1639445441428.jpg'}
+        {name:'Castlevania: Symphony of the Night',img:'https://assets-prd.ignimgs.com/2021/12/14/castlevaniasymp-1639445441428.jpg'},
+        {name:'Batman: Arkham City',img:'https://cdn.europosters.eu/image/1300/posters/batman-arkham-city-cover-i11510.jpg'},
+        {name:'The Evil Within 2',img:'https://freshcomics.s3.amazonaws.com/issue_covers/JUL172131.jpg'},
+        {name:'Final Fantasy VII',img:'https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg'}
     ]
 
   return (

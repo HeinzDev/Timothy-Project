b3b377a1981fdad379f0b82277791a65f27a5f8e
HeinzDev <heinz1dev@gmail.com>
2023-10-25 23:53:04 -0300

    Fixed the array useEffect bug

--- frontend/src/components/games/games.tsx
+++ frontend/src/components/games/games.tsx
@@ -12,24 +12,7 @@ export const Games = () => {
   const [loading, setLoading] = useState<boolean>(true); // Estado de carregamento
   const [data, setData] = useState<GameData[]>([]);
   //api fetch here
-  const array = [
-    { name: 'Catherine: Fullbody', img: 'https://m.media-amazon.com/images/I/815BAHe2-lL.jpg' },
-    { name: 'The killer is dead', img: 'https://m.media-amazon.com/images/I/71MW+LprsjL._AC_UF1000,1000_QL80_.jpg' },
-    {
-      name: 'The Legend of Zelda: Ocarina of time',
-      img: 'https://neverendingrealm.com/wp-content/uploads/2019/02/The-Legend-of-Zelda-Ocarina-of-Time-3D-Box-Art.jpg',
-    },
-    {
-      name: 'Castlevania: Symphony of the Night',
-      img: 'https://assets-prd.ignimgs.com/2021/12/14/castlevaniasymp-1639445441428.jpg',
-    },
-    {
-      name: 'Batman: Arkham City',
-      img: 'https://cdn.europosters.eu/image/1300/posters/batman-arkham-city-cover-i11510.jpg',
-    },
-    { name: 'The Evil Within 2', img: 'https://freshcomics.s3.amazonaws.com/issue_covers/JUL172131.jpg' },
-    { name: 'Final Fantasy VII', img: 'https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg' },
-  ];
+  //const array = fetch()
 
   useEffect(() => {
     //Fake requisition
@@ -44,6 +27,29 @@ export const Games = () => {
       setData(array);
       setLoading(false);
     }, 3000);*/
+
+    // remove this further
+    const array = [
+      { name: 'Catherine: Fullbody', img: 'https://m.media-amazon.com/images/I/815BAHe2-lL.jpg' },
+      { name: 'The killer is dead', img: 'https://m.media-amazon.com/images/I/71MW+LprsjL._AC_UF1000,1000_QL80_.jpg' },
+      {
+        name: 'The Legend of Zelda: Ocarina of time',
+        img: 'https://neverendingrealm.com/wp-content/uploads/2019/02/The-Legend-of-Zelda-Ocarina-of-Time-3D-Box-Art.jpg',
+      },
+      {
+        name: 'Castlevania: Symphony of the Night',
+        img: 'https://assets-prd.ignimgs.com/2021/12/14/castlevaniasymp-1639445441428.jpg',
+      },
+      {
+        name: 'Batman: Arkham City',
+        img: 'https://cdn.europosters.eu/image/1300/posters/batman-arkham-city-cover-i11510.jpg',
+      },
+      { name: 'The Evil Within 2', img: 'https://freshcomics.s3.amazonaws.com/issue_covers/JUL172131.jpg' },
+      {
+        name: 'Final Fantasy VII',
+        img: 'https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg',
+      },
+    ];
 
     setData(array);
     setLoading(false);
